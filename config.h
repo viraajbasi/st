20817553f73253012ddd6c7e5c76dca7e6edd26c
Viraaj Basi <viraajbasi@gmail.com>
2022-05-18 21:57:06 +0100

    adjusted font size

--- config.h
+++ config.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "Caskaydia Cove Nerd Font Mono:pixelsize=18:antialias=true:autohint=true";
+static char *font = "Caskaydia Cove Nerd Font Mono:pixelsize=20:antialias=true:autohint=true";
 static int borderpx = 2;
 
 /*
