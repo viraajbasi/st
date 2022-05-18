1fd3bae4a327b8a9b646655397af91b6aceb9526
Viraaj Basi <viraajbasi@gmail.com>
2022-05-18 11:58:46 +0100

    added alpha patch

--- config.h
+++ config.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "Caskaydia Cove Nerd Font Mono:pixelsize=16:antialias=true:autohint=true";
+static char *font = "Caskaydia Cove Nerd Font Mono:pixelsize=18:antialias=true:autohint=true";
 static int borderpx = 2;
 
 /*
@@ -92,6 +92,9 @@ char *termname = "st-256color";
  *	stty tabs
  */
 unsigned int tabspaces = 8;
+
+/* bg opacity */
+float alpha = 0.8;
 
 /* Terminal colors (16 first used in escape sequence) */
 static const char *colorname[] = {
