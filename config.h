7d1f2340ea68d5d5dd2d35c873f833c92a845c5c
Viraaj Basi <viraajbasi@gmail.com>
2022-05-31 20:58:02 +0100

    adjusted font size

--- config.h
+++ config.h
@@ -7,12 +7,12 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "JetBrainsMono Nerd Font:pixelsize=20:antialias=true:autohint=true";
+static char *font = "JetBrainsMono Nerd Font:pixelsize=22:antialias=true:autohint=true";
 /* Spare fonts */
 static char *font2[] = {
 /*     "Inconsolata for Powerline:pixelsize=12:antialias=true:autohint=true", */
 /*     "Hack Nerd Font Mono:pixelsize=11:antialias=true:autohint=true", */
-    "JoyPixels:pixelsize=20:antialias=true:autohint=true"
+    "JoyPixels:pixelsize=22:antialias=true:autohint=true"
 };
 
 static int borderpx = 2;
