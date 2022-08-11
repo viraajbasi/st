af4e1863056c53e09e4ec68daab5c4cad18a91a2
Viraaj Basi <viraajbasi@gmail.com>
2022-08-11 17:58:00 +0100

    updated config.def.h

--- config.h
+++ config.h
@@ -1,18 +1,19 @@
 /* See LICENSE file for copyright and license details. */
 
-#include "/home/viraaj/.cache/wal/colors-wal-st.h"
+//#include "colors-wal-st.h"
 
 /*
  * appearance
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "JetBrainsMono Nerd Font:pixelsize=18:antialias=true:autohint=true";
+static char *font = "JetBrains Mono:pixelsize=18:antialias=true:autohint=true";
 /* Spare fonts */
 static char *font2[] = {
 /*     "Inconsolata for Powerline:pixelsize=12:antialias=true:autohint=true", */
 /*     "Hack Nerd Font Mono:pixelsize=11:antialias=true:autohint=true", */
-    "JoyPixels:pixelsize=18:antialias=true:autohint=true"
+    "JoyPixels:pixelsize=18:antialias=true:autohint=true",
+    "Symbols Nerd Font:pixelsize=18:antialias=true:autohint=true",
 };
 
 static int borderpx = 2;
@@ -102,8 +103,49 @@ char *termname = "st-256color";
  */
 unsigned int tabspaces = 8;
 
+/* Terminal colors (16 first used in escape sequence) */
+static const char *colorname[] = {
+	/* 8 normal colors */
+	"black",
+	"red3",
+	"green3",
+	"yellow3",
+	"blue2",
+	"magenta3",
+	"cyan3",
+	"gray90",
+
+	/* 8 bright colors */
+	"gray50",
+	"red",
+	"green",
+	"yellow",
+	"#5c5cff",
+	"magenta",
+	"cyan",
+	"white",
+
+	[255] = 0,
+
+	/* more colors can be added after 255 to use with DefaultXX */
+	"#cccccc",
+	"#555555",
+	"gray90", /* default foreground colour */
+	"black", /* default background colour */
+};
+
+
+/*
+ * Default colors (colorname index)
+ * foreground, background, cursor, reverse cursor
+ */
+unsigned int defaultfg = 258;
+unsigned int defaultbg = 259;
+unsigned int defaultcs = 256;
+static unsigned int defaultrcs = 257;
+
 /* bg opacity */
-float alpha = 0.8;
+float alpha = 0.6;
 
 
 /*
